cf0642c76895ff125709c82551fba1de050d6c7c
Luke Fraser <lukefrasera@gmail.com>
2014-12-16 16:45:50 -0800

    adding opengl support

--- include/doodleview.h
+++ include/doodleview.h
@@ -7,10 +7,10 @@
 #include "btBulletCollisionCommon.h"
 #include "btBulletDynamicsCommon.h"
 #define GLM_FORCE_RADIANS
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 namespace doodle {
 
